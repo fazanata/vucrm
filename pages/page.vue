8e33e40c3b5ecdecd27276b67a062074d45322dc
fazanata <faz0@ya.ru>
2021-03-31 23:24:42 +0400

    first commit

---
+++ pages/page.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        1233455
+    </div>
+</template>
+
+<script>
+
+export default {
+    head() {
+        return {
+            link:[
+                {rel: "canonical", href: `http://localhost:3000${this.$route.path}`}
+            ],
+            title: "текстовый тайтл",
+            meta: [
+                {hid: "description", name: "dewscription", content: "текстовый декс"},
+                {hid: "robots", name: "robots", content: "noindex, nofollow"}
+            ]
+        }
+    },
+    mounted: function () {
+        console.log(this.$route.path)
+    }
+}
+</script>
